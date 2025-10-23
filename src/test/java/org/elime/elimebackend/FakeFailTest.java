b8589633efe77500d56956d91a132973fb5ebe6a
FrancescoCavaliere11 <francescounical@gmail.com>
2025-10-23 21:27:42 +0200

    commit per testare CI

--- src/test/java/org/elime/elimebackend/FakeFailTest.java
+++ src/test/java/org/elime/elimebackend/FakeFailTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import static com.jayway.jsonpath.internal.path.PathCompiler.fail;
 
 public class FakeFailTest {
+    private int i = 0;
     @Test
     void testThatAlwaysFails() {
         fail("Questo test fallisce intenzionalmente per testare la CI");
