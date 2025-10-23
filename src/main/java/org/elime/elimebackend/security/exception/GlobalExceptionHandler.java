11673b0a4328981e88c678f2848bac32dc049c08
FrancescoCavaliere11 <francescounical@gmail.com>
2025-10-23 23:37:16 +0200

    modificato GlobalExceptionHandler

--- src/main/java/org/elime/elimebackend/security/exception/GlobalExceptionHandler.java
+++ src/main/java/org/elime/elimebackend/security/exception/GlobalExceptionHandler.java
@@ -16,7 +16,7 @@ import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    int i = 1234;
+    int i = 4321;
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorDto handleValidationExceptions(HttpServletRequest request, MethodArgumentNotValidException e) {
